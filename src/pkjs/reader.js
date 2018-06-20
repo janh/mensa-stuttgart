6f03274d185ccf41d7a8943c4fca381527cb8713
Jan Hoffmann <jan@3e8.eu>
2018-06-20 16:24:01 +0200

    Fix parsing of buffet price

--- src/pkjs/reader.js
+++ src/pkjs/reader.js
@@ -180,6 +180,7 @@ function parseMeal(row) {
 
         var prices = priceColumn.findElementsByName('div')[0].content();
         prices = prices.replace(/[€\s]/g, '');
+        prices = prices.split('(')[0].trim();
         prices = prices.split('/');
 
         var priceStudent = prices[0];
